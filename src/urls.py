5ded478901da9726927820645e05d4808cc94ed8
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-09-18 13:39:56 +0300

    Url mapping for public API.

--- src/urls.py
+++ src/urls.py
@@ -5,12 +5,17 @@ from wheezy.routing import url
 
 from public.web.urls import error_urls
 from public.web.urls import public_urls
+from public.web.urls import public_api_urls
 from public.web.urls import static_urls
 from public.web.views import welcome
 
 
+all_api_urls = []
+all_api_urls += public_api_urls
+
 all_urls = [
-    url('', welcome, {'path': 'index.html'}, name='default')
+    url('', welcome, {'path': 'index.html'}, name='default'),
+    url('api/v1/', all_api_urls)
 ]
 all_urls += public_urls
 all_urls += [('error/', error_urls)]
