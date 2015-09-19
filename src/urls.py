d26f2e5e629030ccc6520d2620c5c5789309125c
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-09-19 19:01:57 +0300

    Added membership url mapping.

--- src/urls.py
+++ src/urls.py
@@ -3,20 +3,24 @@
 
 from wheezy.routing import url
 
+from membership.web.urls import membership_api_urls
+from membership.web.urls import membership_urls
 from public.web.urls import error_urls
-from public.web.urls import public_urls
 from public.web.urls import public_api_urls
+from public.web.urls import public_urls
 from public.web.urls import static_urls
-from public.web.views import welcome
+from shared.views import url_index
 
 
 all_api_urls = []
 all_api_urls += public_api_urls
+all_api_urls += membership_api_urls
 
 all_urls = [
-    url('', welcome, {'path': 'index.html'}, name='default'),
+    url_index('', name='default'),
     url('api/v1/', all_api_urls)
 ]
+all_urls += membership_urls
 all_urls += public_urls
 all_urls += [('error/', error_urls)]
 all_urls += static_urls
