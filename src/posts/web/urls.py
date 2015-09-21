da66d2014166618fe2818b9a89b908665638a352
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-09-21 19:31:31 +0300

    Added post comments handler.

--- src/posts/web/urls.py
+++ src/posts/web/urls.py
@@ -6,12 +6,14 @@ from wheezy.routing import url
 from shared.views import url_index
 
 from posts.web.views import PostHandler
+from posts.web.views import PostCommentsHandler
 from posts.web.views import SearchPostsHandler
 
 
 posts_api_urls = [
     url('search/posts', SearchPostsHandler),
-    url('post/{slug}', PostHandler)
+    url('post/{slug}', PostHandler),
+    url('post/{slug}/comments', PostCommentsHandler)
 ]
 
 posts_urls = [
--- src/posts/web/views.py
+++ src/posts/web/views.py
@@ -4,9 +4,11 @@
 from wheezy.core.collections import attrdict
 from wheezy.core.comp import u
 from wheezy.http.response import not_found
+from wheezy.web.authorization import authorize
 
 from shared.views import APIHandler
 
+from posts.validation import post_comment_validator
 from posts.validation import search_posts_validator
 
 
@@ -35,12 +37,34 @@ class PostHandler(APIHandler):
 
     def get_post(self, slug, fields):
         with self.factory('ro') as f:
-            p = f.posts.get_post(slug)
-            if not p:
+            r = f.posts.get_post(slug)
+            if not r:
                 return None
+            p, post_id = r
             if fields:
                 if 'permissions' in fields:
-                    p['permissions'] = f.posts.post_permissions(p['id'])
+                    p['permissions'] = f.posts.post_permissions(post_id)
                 if 'comments' in fields:
-                    p['comments'] = f.posts.list_comments(p['id'])
+                    p['comments'] = f.posts.list_comments(post_id)
         return p
+
+
+class PostCommentsHandler(APIHandler):
+
+    @authorize
+    def post(self):
+        m = attrdict(slug=self.route_args.slug, message=u(''))
+        if (not self.try_update_model(m) or
+                not self.validate(m, post_comment_validator) or
+                not self.add_post_comment(m)):
+            return self.json_errors()
+        r = self.json_response({})
+        r.status_code = 201
+        return r
+
+    def add_post_comment(self, m):
+        with self.factory('rw') as f:
+            if not f.posts.add_post_comment(m.slug, m.message):
+                return False
+            f.session.commit()
+        return True
