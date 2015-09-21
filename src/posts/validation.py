d928672ff855cbe0a223a47684dc025b06fec2b0
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-09-21 19:27:08 +0300

    Added post comment validator.

--- src/posts/validation.py
+++ src/posts/validation.py
@@ -3,9 +3,14 @@
 
 from wheezy.validation import Validator
 from wheezy.validation.rules import length
+from wheezy.validation.rules import required
 
 
 search_posts_validator = Validator({
     'q': [length(max=20)],
     'page': []
+})
+
+post_comment_validator = Validator({
+    'message': [required, length(min=2), length(max=250)]
 })
