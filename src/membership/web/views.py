c46ebd30a6d5ee500ed904904dbb3a3aa19cfc1e
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-09-22 19:08:44 +0300

    Added validation checks to signup web handler; minor readability cleanup in signout web handler.

--- src/membership/web/views.py
+++ src/membership/web/views.py
@@ -9,6 +9,8 @@ from wheezy.web.authorization import authorize
 from shared.views import APIHandler
 
 from membership.validation import credential_validator
+from membership.validation import password_match_validator
+from membership.validation import signup_validator
 
 
 class SignInHandler(APIHandler):
@@ -26,7 +28,7 @@ class SignInHandler(APIHandler):
             user = f.membership.authenticate(credential)
             if not user:
                 return False
-        self.principal = Principal(id=user['id'])
+        self.principal = Principal(id=user.id)
         return True
 
 
@@ -34,14 +36,22 @@ class SignUpHandler(APIHandler):
 
     def post(self):
         m = attrdict(email=u(''), username=u(''), password=u(''))
-        m.update(self.request.form)
-        if not self.signup(m):
+        p = attrdict(password=u(''), confirm_password=u(''))
+        if (not self.try_update_model(m) &
+                self.try_update_model(p) or
+                not self.validate(m, credential_validator) &
+                self.validate(m, signup_validator) &
+                self.validate(p, password_match_validator) or
+                not self.create_account(m)):
             return self.json_errors()
         return self.json_response({})
 
-    def signup(self, m):
-        self.error('This feature is not available yet.')
-        return False
+    def create_account(self, m):
+        with self.factory('rw') as f:
+            if not f.membership.create_account(m):
+                return False
+            f.session.commit()
+        return True
 
 
 class SignOutHandler(APIHandler):
@@ -55,4 +65,9 @@ class UserHandler(APIHandler):
 
     @authorize
     def get(self):
-        return self.json_response({'username': self.principal.id})
+        u = self.get_user()
+        return self.json_response({'username': u.username})
+
+    def get_user(self):
+        with self.factory('ro') as f:
+            return f.membership.user
