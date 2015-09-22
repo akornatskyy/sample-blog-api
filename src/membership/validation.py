2870c70ab069b7d493e79ddba87886497009cd84
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-09-22 19:05:32 +0300

    Added password match and signup validators.

--- src/membership/validation.py
+++ src/membership/validation.py
@@ -2,6 +2,8 @@
 """
 
 from wheezy.validation import Validator
+from wheezy.validation.rules import compare
+from wheezy.validation.rules import email
 from wheezy.validation.rules import length
 from wheezy.validation.rules import required
 
@@ -11,4 +13,18 @@ from membership.rules import password_rules
 credential_validator = Validator({
     'username': [required, length(min=2), length(max=20)],
     'password': password_rules
+})
+
+password_match_validator = Validator({
+    'password': [
+        compare(
+            equal='confirm_password',
+            message_template='Passwords do not match.')
+    ]
+})
+
+signup_validator = Validator({
+    'email': [required, length(min=6), length(max=30), email],
+    # 'first_name': [required, length(max=30)],
+    # 'last_name': [required, length(max=30)]
 })
