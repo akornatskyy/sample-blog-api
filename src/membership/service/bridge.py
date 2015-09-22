c3b12950057ba5e2f21ad830ffb5480752e01337
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-09-22 19:07:19 +0300

    Added create account to membership service bridge.

--- src/membership/service/bridge.py
+++ src/membership/service/bridge.py
@@ -1,18 +1,40 @@
 """
 """
 
+from wheezy.core.descriptors import attribute
+from wheezy.security.authorization import authorized
 from wheezy.validation.mixin import ErrorsMixin
 
 
 class MembershipService(ErrorsMixin):
 
-    def __init__(self, factory, errors):
+    def __init__(self, factory, errors, principal):
         self.factory = factory
         self.errors = errors
+        self.principal = principal
+
+    @attribute
+    @authorized
+    def user(self):
+        return self.factory.membership.get_user(self.principal.id)
 
     def authenticate(self, credential):
         up = self.factory.membership.authenticate(credential.username.lower())
-        if not up or up['password'] != credential.password:
+        if not up or up.password != credential.password:
             self.error('The username or password provided is incorrect.')
             return None
-        return self.factory.membership.get_user(up['id'])
+        return self.factory.membership.get_user(up.id)
+
+    def create_account(self, registration):
+        if self.factory.membership.has_account(registration.username):
+            self.error(
+                'The user with such username is already registered. '
+                'Please try another.',
+                name='username')
+            return False
+        if not self.factory.membership.create_account(registration):
+            self.error(
+                'The system was unable to create an account for you. '
+                'Please try again later.')
+            return False
+        return True
