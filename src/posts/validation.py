b1c0176b0681c2ce1e6a3dabbf3f9ae200f2945c
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-09-22 16:35:10 +0300

    Added slug validation rule; improved search posts validator; added post spec validator.

--- src/posts/validation.py
+++ src/posts/validation.py
@@ -2,15 +2,26 @@
 """
 
 from wheezy.validation import Validator
+from wheezy.validation.rules import iterator
 from wheezy.validation.rules import length
+from wheezy.validation.rules import one_of
+from wheezy.validation.rules import range
 from wheezy.validation.rules import required
 
 
+slug_rules = [required, length(min=2), length(max=35)]
+
 search_posts_validator = Validator({
     'q': [length(max=20)],
-    'page': []
+    'page': [range(min=0), range(max=9)]
+})
+
+post_spec_validator = Validator({
+    'slug': slug_rules,
+    'fields': [iterator([one_of(('', 'permissions', 'comments'))])]
 })
 
 post_comment_validator = Validator({
+    'slug': slug_rules,
     'message': [required, length(min=2), length(max=250)]
 })
