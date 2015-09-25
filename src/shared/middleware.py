cc20e184bc89ef0c5d327854cdb33f7f0b46876d
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-09-25 22:17:02 +0300

    Multiple status code support for API http error pass through middleware.

--- src/shared/middleware.py
+++ src/shared/middleware.py
@@ -4,19 +4,18 @@
 
 class HTTPErrorPassThroughMiddleware(object):
 
-    def __init__(self, status_code, route_name):
-        self.status_code = status_code
-        self.route_name = route_name
+    def __init__(self, http_errors):
+        self.http_errors = http_errors
 
     def __call__(self, request, following):
         response = following(request)
-        if (response.status_code == self.status_code and
+        if (response.status_code >= 400 and
                 request.ajax and
                 request.path.startswith('/api/')):
-            request.environ['route_args']['route_name'] = self.route_name
+            request.environ['route_args']['route_name'] = \
+                self.http_errors[response.status_code]
         return response
 
 
 def http_error_pass_through_middleware_factory(options):
-    route_name = options['http_errors'][400]
-    return HTTPErrorPassThroughMiddleware(400, route_name)
+    return HTTPErrorPassThroughMiddleware(options['http_errors'])
